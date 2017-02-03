d10033f838ca31a5e641cea14008ce7a3131114d
michalszczepanski91 <michalszczepanski91@gmail.com>
2017-02-03 23:43:53 +0100

    script - run binary with several option

--- run_binary.py
+++ run_binary.py
@@ -1,13 +1,18 @@
 #! /usr/bin/python
 import os
 
+# call for program
 cmd_body = './runMyProgram '
-list_option = ['-s ','-a ','-h ']
-# range to list_option.length
-for i in range (0, 3):
-	cmd = cmd_body + list_option[i]
-# range to patameters.length
+# possible option
+# -n normal
+# -f fast
+# -h help
+list_option = ['-n ','-f ','-h ']
+
+#print list_option.length
+for idx, option in enumerate(list_option):
+	# range to patameters.length
 	for j in range (0, 5):
-		cmd = cmd_body + list_option[i] + str(j)
-		#os.system(cmd)
+		cmd = cmd_body + option +str(j)
+#		os.system(cmd)
 		print (cmd)
